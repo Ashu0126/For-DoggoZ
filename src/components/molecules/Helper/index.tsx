45943033fca7dc8a88d8bd426bcb970228245ca7
Ashu0126 <ashish.jaryal14@gmail.com>
2024-03-21 15:58:07 +0530

    helper component

---
+++ src/components/molecules/Helper/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import FloatingInput from "../../atoms/FloatingInput";
+import Button from "../../atoms/Button";
+import style from "./index.module.scss";
+
+const Helper = () => {
+  return (
+    <div className={style.helperSection}>
+      <h2
+        dangerouslySetInnerHTML={{
+          __html: `Find Trusted <span>Dog Helpers</span> Near You: Your Furry Friend's
+        <span> Best Companion</span> Awaits!`,
+        }}
+      />
+      <div className={style.inputSection}>
+        <FloatingInput label={"Enter your location"} />
+        <Button>search</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Helper;
