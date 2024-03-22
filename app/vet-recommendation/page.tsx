0cdfc86f6f86cb66d95f20594bbd111822c3d746
Ashu0126 <ashish.jaryal14@gmail.com>
2024-03-22 14:47:05 +0530

    vet form refactor

--- app/vet-recommendation/page.tsx
+++ app/vet-recommendation/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 import style from "./page.module.scss";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { fetchResult } from "@/src/utils/fetchApi";
 import FloatingInput from "@/src/components/atoms/FloatingInput";
 import Button from "@/src/components/atoms/Button";
+import Footer from "@/src/components/molecules/Footer";
+import pageData from "./../../data/vet.json";
 
 const Page = () => {
   const [result, setResult] = useState<any>({});
   const [shift, setShift] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [type, setType] = useState("");
+
+  useEffect(() => {
+    setType(window.location.pathname.split("/")[1].split("-")[0]);
+  }, []);
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -36,32 +44,81 @@ const Page = () => {
   }, [result]);
 
   return (
-    <div className={style.page}>
-      <div className={style.container}>
-        <div className={`${style.formContainer} ${shift ? style.slide : ""}`}>
-          <div className={style.logo}>
-            <img src="/svg/logo.svg" alt="" />
-            <h3>ForDoggoZ</h3>
-          </div>
-          <div className={style.head}>
-            <h2>
-              {shift ? "Nearest Vet" : "Find the nearest veterinary clinic"}
-            </h2>
-            {!shift && <p>Fill the form below.</p>}
-            {shift && <h3>{result?.nearestVet}</h3>}
-          </div>
-          <form onSubmit={handleSubmit}>
-            {!shift && (
+    <>
+      <div className={`${style.page} ${type ? style[type] : ""}`}>
+        <div className={style.container}>
+          <div className={`${style.formContainer} ${shift ? style.slide : ""}`}>
+            <div className={style.logo}>
+              <img src={pageData?.logo?.icon} alt="" />
+              <h3>{pageData?.logo?.text}</h3>
+            </div>
+            {!shift ? (
               <>
-                <FloatingInput label={"Enter city"} name={"city"} />
-                <FloatingInput label={"Enter sector"} name={"sector"} />
+                <div className={style.head}>
+                  <h2>{pageData?.formData?.heading}</h2>
+                  <p>{pageData?.formData?.subHeading}</p>
+                </div>
+                <form onSubmit={handleSubmit}>
+                  {!shift && (
+                    <>
+                      {/* <SelectDropDown
+                    label={"Select city"}
+                    options={["Delhi", "Faridabad", "Gurgaon", "Noida"]}
+                  /> */}
+                      {pageData?.formData?.inputs?.map((input: any) => (
+                        <Fragment key={input?.name}>
+                          <FloatingInput
+                            label={input?.label}
+                            name={input?.name}
+                          />
+                        </Fragment>
+                      ))}
+                    </>
+                  )}
+                  <Button>{pageData?.formData?.btnText}</Button>
+                </form>
               </>
+            ) : (
+              <div className={style.resultSection}>
+                <Button onClick={() => setShift(!shift)}>
+                  <img src={pageData?.resultSection?.btnIcon} alt="" />
+                </Button>
+                <div className={style.result}>
+                  <h2>{pageData?.resultSection?.heading}</h2>
+                  <h3>{result?.nearestVet}</h3>
+                </div>
+              </div>
             )}
-            <Button>{shift ? "Go Back" : "Find Vet"}</Button>
-          </form>
+          </div>
+          <div
+            className={`${style.carouselContainer} ${
+              shift ? style.carouselSlide : ""
+            }`}
+          >
+            <div className={style.carousel}>
+              {pageData?.carousel?.map((item: any, index: any) => (
+                <img
+                  key={`_${index}`}
+                  src={item}
+                  className={activeIndex === index ? style.show : ""}
+                  alt=""
+                />
+              ))}
+            </div>
+            <div className={style.carouselBtn}>
+              {pageData?.carousel?.map((_: any, index: any) => (
+                <span
+                  key={`_${index}`}
+                  onClick={() => setActiveIndex(index)}
+                  className={activeIndex === index ? style.active : ""}
+                ></span>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
